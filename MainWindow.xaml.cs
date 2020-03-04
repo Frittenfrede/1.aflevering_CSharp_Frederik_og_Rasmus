f3e0d389b9156fbe83e485825f1d067b092d2880
Frederik Sørensen <frederiksorensen1@live.dk>
2020-03-04 15:25:56 +0100

    Initial commit -- Rasmus og Frederik

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Aflevering
+{
+    //Opens a window that allows the user to choose a txtfile
+    public class OpenFileDialogForm : Form
+    {
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+
+        // Constructor for OpenDialogForm
+        public OpenFileDialogForm()
+        {
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog()
+            {
+                FileName = "Select a text file",
+                Filter = "Text files (*.txt)|*.txt",
+                Title = "Open text file"
+            };
+        }
+
+        public void OpenFileDialog(object sender, RoutedEventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    List<Person> persons;
+                    //Read file
+                    using (Stream str = openFileDialog1.OpenFile())
+                    {
+                       persons = Person.ReadCSVFile(openFileDialog1.FileName);
+                    }
+                    //Add persons to the list
+                    if(persons != null)
+                    {
+                        MainWindow.persons.Clear();
+                      foreach(Person p in persons)
+                        {
+                            MainWindow.persons.Add(p);
+                        }
+                    } 
+                }
+                catch (SecurityException ex)
+                {
+                    System.Windows.MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public static ObservableCollection<Person> persons = new ObservableCollection<Person>();
+ 
+        //Main window initialize
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbpersons.ItemsSource = persons;
+        }
+        //Opens dialogform for loading txtfile and changes the content of the statuslabel
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialogForm form = new OpenFileDialogForm();
+            form.OpenFileDialog(sender, e);
+            DateTime loadTime = DateTime.Now;
+            listStatusLabel.Content = "File loaded: " + loadTime + " - Number of people in the list: " + lbpersons.Items.Count;
+        }
+
+        //Updates the datacontent for detailed person information
+        private void lbpersons_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lbpersons.Items.Refresh();
+            innerGrid.DataContext = lbpersons.SelectedItem;
+        }
+    }
+    }
+
---
+++ Person.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Aflevering
+{
+    public class Person : INotifyPropertyChanged
+    {
+        // OberservableCollection which contains the persons loaded in from the textfile
+        private static ObservableCollection<Person> persons = new ObservableCollection<Person>();
+        private int id;
+        private String name;
+        private int age;
+        private int score;
+
+        //data is the content from the selected textfile
+        //data is splitted and parsed into the person constructor
+        public Person(string data)
+        {
+            try
+            {
+
+                var L = data.Split(';');
+                Id = int.Parse(L[0]);
+                Name = L[1];
+                Age = int.Parse(L[2]);
+                Score = int.Parse(L[3]);
+            }
+            catch (Exception e) { };
+        }
+
+        public string Name
+        {
+            set
+            {
+                name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+            get { return name; }
+        }
+
+        public int Id
+        {
+            set
+            {
+                id = value;
+                NotifyPropertyChanged(nameof(id));
+            }
+            get
+            {
+                return id;
+            }
+        }
+
+        public int Age
+        {
+            set
+            {
+                age = value;
+                NotifyPropertyChanged(nameof(Age));
+            }
+            get
+            {
+                return age;
+            }
+        }
+
+        public int Score
+        {
+            set
+            {
+                score = value;
+                NotifyPropertyChanged(nameof(Score));
+            }
+            get
+            {
+                return score;
+            }
+        }
+
+
+        public override string ToString()
+        {
+            return Name +" " + age;      
+        }
+
+        //Load txt file
+        public static List<Person> ReadCSVFile(string path)
+        {
+            List<Person> list = new List<Person>();
+            using (var file = new StreamReader(path))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    var p = new Person(line);
+                    list.Add(p);
+                    Console.WriteLine(p);
+                }
+            }
+            return list;
+        }
+
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //Notifies the property owner when the property is changed
+        //Essential for working with binding
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            
+        }
+
+    }
+}
+
